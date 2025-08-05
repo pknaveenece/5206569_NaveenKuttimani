fff912c1a2729896c511f5855e100b95240054ce
pknaveenece <pk.naveenece@gmail.com>
2025-08-05 12:43:54 +0530

    Update Plus_Minus.c

--- HackerRank/Week1/Plus_Minus.c
+++ HackerRank/Week1/Plus_Minus.c
@@ -1,3 +1,6 @@
+
+// Link :https://www.hackerrank.com/challenges/one-month-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+
 #include<stdio.h>
 int main(){
     int size, arr[100];
